66f26b093a92005750192d4b961ce6a3084d6163
Mickdevil <trxctvolka@gmail.com>
2021-06-07 13:08:44 +0200

    the hangedMan game with C

---
+++ cmake-build-debug/CMakeFiles/fivthWorkSpace.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fivthWorkSpace.dir/main.c.obj"
+  "fivthWorkSpace.exe"
+  "fivthWorkSpace.exe.manifest"
+  "fivthWorkSpace.pdb"
+  "libfivthWorkSpace.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fivthWorkSpace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
